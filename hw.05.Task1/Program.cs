357a584f6651eb9268e537c238baf0783526aa8c
name <@DESKTOP-NOAOHEE>
2021-02-20 17:43:21 +0300

    HW.05

---
+++ hw.05.Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hw._05.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int b = Convert.ToInt16(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i <= b; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Сумма чисел от " + 1 + " до " + b + ": " + sum);
+            Console.ReadKey();
+        }
+    }
+}
